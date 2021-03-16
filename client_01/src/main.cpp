5cf5ace32cccbb92367e4e6d401e49cd511def6c
Sofie Verschueren <verschueren.sofie@telenet.be>
2021-03-16 18:27:45 +0100

    Gecombineerde code werkt

--- client_01/src/main.cpp
+++ client_01/src/main.cpp
@@ -3,18 +3,13 @@
 #include <RH_RF95.h>
 #include <RHReliableDatagram.h>
 
-#define CLIENT_ADDRESS 2
-#define SERVER_ADDRESS 1
+#define MEASUREMENT_PERIOD  5000
 
-byte PIN_SENSOR   = 10;
-// The hall-effect flow sensor outputs approximately 4.5 pulses per second per
-// litre/minute of flow.
-float calibrationFactor = 98;
-volatile uint16_t pulseCount;  
-float flowRate;
-unsigned int flowMilliLitres;
-unsigned long totalMilliLitres;
-unsigned long oldTime;
+#define CLIENT_ADDRESS      2
+#define SERVER_ADDRESS      1
+
+#define PIN_SENSOR          10
+#define PIN_3V3_ENABLE      8
 
 // Singleton instance of the radio driver
 RH_RF95 driver(6, 2);
@@ -22,40 +17,103 @@ RH_RF95 driver(6, 2);
 // Class to manage message delivery and receipt, using the driver declared above
 RHReliableDatagram manager(driver, CLIENT_ADDRESS);
 
+// flow rate
+volatile uint16_t pulseCount;  
+float calibrationFactor = 98;
+uint16_t totalMilliLitres;
+unsigned long oldTime=0;
+
 //ISR
 bool prevPulsState = true; 
 bool FlowPuls = false; 
-bool busyCalculating= false; 
-bool busySending= false ;  
+
+// Making an interrupt pin from pin 10 
+ISR (PCINT0_vect){ //  pin change interrupt for D8 to D13
+  PCIFR  |= bit (digitalPinToPCICRbit(PIN_SENSOR)); // clear any outstanding interrupt
+  bool currentState = (bool)digitalRead(PIN_SENSOR);
+  if(prevPulsState){
+    if(!currentState){
+      pulseCount++;
+      FlowPuls = true; 
+    }
+  }
+  prevPulsState = currentState;
+}
+
+bool sendData(uint8_t * data, uint8_t len);
+void calculatingFlow(void);
 
 void setup() 
 {
-  // Dramco uno - enable 3v3 voltage regulator
- pinMode(8, OUTPUT);
-digitalWrite(8, HIGH);
-
   Serial.begin(9600);
   while(!Serial) ; // Wait for serial port to be available
-  if(!manager.init())
-    Serial.println("init failed");
-  driver.setFrequency(868);
+
+  Serial.println(F("I/O init."));
+  // Dramco uno - enable 3v3 voltage regulator
+  pinMode(PIN_3V3_ENABLE, OUTPUT);
+  digitalWrite(PIN_3V3_ENABLE, HIGH);
 
   //Setup Interuptpin 10 
   //pinMode(PIN_SENSOR, INPUT_PULLUP);
   pinMode(PIN_SENSOR, INPUT);
-  *digitalPinToPCMSK(PIN_SENSOR) |= bit (digitalPinToPCMSKbit(PIN_SENSOR));  // enable pin
+  *digitalPinToPCMSK(PIN_SENSOR) |= bit (digitalPinToPCMSKbit(PIN_SENSOR));  // enable pin change interrupt
   PCIFR  |= bit (digitalPinToPCICRbit(PIN_SENSOR)); // clear any outstanding interrupt
   PCICR  |= bit (digitalPinToPCICRbit(PIN_SENSOR)); // enable interrupt for the group
 
+  Serial.println(F("Modem init."));
+  if(!manager.init())
+  {
+    Serial.println("init failed");
+  }
+  
+  driver.setFrequency(868);
+  manager.setRetries(0);
+  manager.setTimeout(100);
+
+  Serial.println(F("Init variables"));
   pulseCount        = 0;
-  flowRate          = 0.0;
-  flowMilliLitres   = 0;
   totalMilliLitres  = 0;
   oldTime           = 0;
+
+  Serial.println(F("Setup complete."));
 }
 
+void loop()
+{
+  if((millis() - oldTime) > MEASUREMENT_PERIOD)    // Only process counters once per minute 
+  {
+    oldTime = millis();
+    calculatingFlow();
+  }
 
-void CalculatingFlow()
+  if (totalMilliLitres > 20 ) // send message each 20ml
+  {
+    uint8_t data[4] = {'d', 'a', 't', 'a'};
+    sendData(data, sizeof(data));
+    totalMilliLitres -= 20;
+  }
+
+  
+
+}
+
+bool sendData(uint8_t * data, uint8_t len){
+  Serial.println(F("Sending to rf95_server"));
+ 
+  // Send a message to manager_server
+  if (manager.sendtoWait(data, len, SERVER_ADDRESS))
+  {
+    Serial.println(F("Data sent, ACK received"));
+    return true;
+  }
+  else
+  {
+    Serial.println(F("No reply, is rf95_server running?"));
+    return false;
+  }
+}
+
+void calculatingFlow(void)
 {
     //Store counter temporarily
     uint16_t pulseCount_temp = pulseCount;
@@ -63,29 +121,14 @@ void CalculatingFlow()
     // Reset the pulse counter so we can start incrementing again 
     pulseCount = 0;
   
-    // Because this loop may not complete in exactly 1 second intervals we calculate
-    // the number of milliseconds that have passed since the last execution and use
-    // that to scale the output. We also apply the calibrationFactor to scale the output
-    // based on the number of pulses per second per units of measure (litres/minute in
-    // this case) coming from the sensor.
-    //flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount_temp) / calibrationFactor;   // calculating per seconde 
-    flowRate = ((60000.0 / (millis() - oldTime)) * pulseCount_temp) / calibrationFactor; // calculating per minute
-
-
-    // Note the time this processing pass was executed. Note that because we've
-    // disabled interrupts the millis() function won't actually be incrementing right
-    // at this point, but it will still return the value it was set to just before
-    // interrupts went away.
-    oldTime = millis();
-    
-    // Divide the flow rate in litres/minute by 60 to determine how many litres have
-    // passed through the sensor in this 1 second interval, then multiply by 1000 to
-    // convert to millilitres.
+    float flowRate =  (float)(pulseCount_temp / calibrationFactor); // calculating per minute
+   
+   
    // flowMilliLitres = (flowRate / 60) * 1000;   // calculating per seconde 
-    flowMilliLitres = flowRate  * 1000;         // calculating per minute
+    float flowMilliLitres = (flowRate *1000  * MEASUREMENT_PERIOD)/ 60000 ;         // calculating per minute
     
     // Add the millilitres passed in this second to the cumulative total
-    totalMilliLitres += flowMilliLitres;
+    totalMilliLitres += (uint16_t)round(flowMilliLitres);
       
     
     // Print the cumulative total of litres flowed since starting
@@ -93,79 +136,3 @@ void CalculatingFlow()
     Serial.print(totalMilliLitres);
     Serial.println("mL"); 
 }
-
-void Sending()
-{
-  Serial.println("Sending to rf95_server");
-  // Send a message to rf95_server
-  uint8_t data[] = "data";
-
-  // Send a message to manager_server
-  if (manager.sendtoWait(data, sizeof(data), SERVER_ADDRESS))
-  {
-  // Now wait for a reply
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
-    uint8_t from;   
-
- if (manager.recvfromAckTimeout(buf, &len, 2000, &from))
-    {
-      Serial.print("got reply from : 0x");
-      Serial.print(from, HEX);
-      Serial.print(": ");
-      Serial.println((char*)buf);
-      totalMilliLitres -=20; 
-    } 
-    else
-    {
-      Serial.println("recv failed");
-    }
-  }
-  else
-  {
-    Serial.println("No reply, is rf95_server running?");
-  }  
-}
-
-// Making an interrupt pin from pin 10 
-ISR (PCINT0_vect){ //  pin change interrupt for D8 to D13
-  PCIFR  |= bit (digitalPinToPCICRbit(PIN_SENSOR)); // clear any outstanding interrupt
-  bool currentState = (bool)digitalRead(PIN_SENSOR);
-  if(!FlowPuls && prevPulsState){
-    if(!currentState){
-      pulseCount++;
-      FlowPuls = true; 
-    }
-  }
-  // Only nescessary for debouncing (when making puls by hand)
-  else if (FlowPuls && !prevPulsState){
-    if(currentState){
-      FlowPuls = false; 
-    }
-  }
-  prevPulsState = currentState;
-}
-
-#warning "compiling lora ptp client code"
-void loop()
-{
- //if((millis() - oldTime) > 1000 && !busySending)    // Only process counters once per second
- if((millis() - oldTime) > 60000 && !busySending)    // Only process counters once per minute 
-  {
-    busyCalculating= true;
-    CalculatingFlow();
-    busyCalculating= false;
-  }
-
-  if (totalMilliLitres > 20 && !busyCalculating) // send message each 20ml
-  {
-    //noInterrupts ();
-    busySending= true ;
-    Sending(); 
-    //interrupts ();
-    busySending= false ;
-  }
-}
-
-
-

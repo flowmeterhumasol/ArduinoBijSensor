c518e3656dc52b983968001a50348fa7d9120c06
Sofie Verschueren <verschueren.sofie@telenet.be>
2021-03-17 14:45:38 +0100

    CodeWerkt

--- client_01/src/main.cpp
+++ client_01/src/main.cpp
@@ -4,7 +4,6 @@
 #include <RHReliableDatagram.h>
 
 #define MEASUREMENT_PERIOD  5000
-#define SENDING_THRESHOLD   25 
 
 #define CLIENT_ADDRESS      2
 #define SERVER_ADDRESS      1
@@ -12,9 +11,9 @@
 #define PIN_SENSOR          10
 #define PIN_3V3_ENABLE      8
 
-//Modem
 // Singleton instance of the radio driver
 RH_RF95 driver(6, 2);
+
 // Class to manage message delivery and receipt, using the driver declared above
 RHReliableDatagram manager(driver, CLIENT_ADDRESS);
 
@@ -31,68 +30,74 @@ bool prevPulsState = true;
 ISR (PCINT0_vect){ //  pin change interrupt for D8 to D13
   PCIFR  |= bit (digitalPinToPCICRbit(PIN_SENSOR)); // clear any outstanding interrupt
   bool currentState = (bool)digitalRead(PIN_SENSOR);
-  if(prevPulsState && !currentState){
+  if(prevPulsState){
+    if(!currentState){
       pulseCount++;
-      Serial.print(F("."));
+    }
   }
   prevPulsState = currentState;
 }
-
 
 bool sendData(uint8_t * data, uint8_t len);
 void calculatingFlow(void);
 
 void setup() 
 {
-  //Init variables
-  pulseCount        = 0;
-  totalMilliLitres  = 0;
-  oldTime           = 0;
+  Serial.begin(9600);
+  while(!Serial) ; // Wait for serial port to be available
 
-  // I/O init
+  Serial.println(F("I/O init."));
   // Dramco uno - enable 3v3 voltage regulator
   pinMode(PIN_3V3_ENABLE, OUTPUT);
   digitalWrite(PIN_3V3_ENABLE, HIGH);
 
-  Serial.begin(9600);
-  while(!Serial) ; // Wait for serial port to be available
+  //Setup Interuptpin 10 
+  //pinMode(PIN_SENSOR, INPUT_PULLUP);
+  pinMode(PIN_SENSOR, INPUT);
+  *digitalPinToPCMSK(PIN_SENSOR) |= bit (digitalPinToPCMSKbit(PIN_SENSOR));  // enable pin change interrupt
+  PCIFR  |= bit (digitalPinToPCICRbit(PIN_SENSOR)); // clear any outstanding interrupt
+  PCICR  |= bit (digitalPinToPCICRbit(PIN_SENSOR)); // enable interrupt for the group
 
   Serial.println(F("Modem init."));
   if(!manager.init())
   {
     Serial.println("init failed");
   }
+  
   driver.setFrequency(868);
   manager.setRetries(0);
   manager.setTimeout(100);
 
-  //Setup Interuptpin 10 
-  pinMode(PIN_SENSOR, INPUT);
-  *digitalPinToPCMSK(PIN_SENSOR) |= bit (digitalPinToPCMSKbit(PIN_SENSOR));  // enable pin change interrupt
-  PCIFR  |= bit (digitalPinToPCICRbit(PIN_SENSOR)); // clear any outstanding interrupt
-  PCICR  |= bit (digitalPinToPCICRbit(PIN_SENSOR)); // enable interrupt for the group
+  Serial.println(F("Init variables"));
+  pulseCount        = 0;
+  totalMilliLitres  = 0;
+  oldTime           = 0;
 
   Serial.println(F("Setup complete."));
 }
 
 void loop()
 {
-  if((millis() - oldTime) > MEASUREMENT_PERIOD)     
+  if((millis() - oldTime) > MEASUREMENT_PERIOD)    // Only process counters once per minute 
   {
     oldTime = millis();
-    calculatingFlow();
+    calculatingFlow(); 
   }
 
-  /*if (totalMilliLitres >= SENDING_THRESHOLD) 
+  if (totalMilliLitres > 20 ) // send message each 20ml
   {
     uint8_t data[] = "data";
     sendData(data, sizeof(data));
-    totalMilliLitres -= SENDING_THRESHOLD;
-  }*/
+    totalMilliLitres -= 20;
+  }
+
+  
+
 }
 
 bool sendData(uint8_t * data, uint8_t len){
   Serial.println(F("Sending to rf95_server"));
+ 
   // Send a message to manager_server
   if (manager.sendtoWait(data, len, SERVER_ADDRESS))
   {
@@ -110,16 +115,22 @@ void calculatingFlow(void)
 {
     //Store counter temporarily
     uint16_t pulseCount_temp = pulseCount;
+
     // Reset the pulse counter so we can start incrementing again 
     pulseCount = 0;
   
-    float flowRate =  (float)(pulseCount_temp / calibrationFactor); 
-    float flowMilliLitres = (flowRate * 1000  * MEASUREMENT_PERIOD)/ 60000 ;        
+    float flowRate =  (float)(pulseCount_temp / calibrationFactor); // calculating per minute
+   
+   
+   // flowMilliLitres = (flowRate / 60) * 1000;   // calculating per seconde 
+    float flowMilliLitres = (flowRate *1000  * MEASUREMENT_PERIOD)/ 60000 ;         // calculating per minute
+    
     // Add the millilitres passed in this second to the cumulative total
     totalMilliLitres += (uint16_t)round(flowMilliLitres);
-  
+      
+    
     // Print the cumulative total of litres flowed since starting
-    Serial.print("Liquid Quantity: ");        
+    Serial.print("Output Liquid Quantity: ");        
     Serial.print(totalMilliLitres);
     Serial.println("mL"); 
 }

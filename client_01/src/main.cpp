106134709609f059315e2763616dbb79b0ae0175
Sofie Verschueren <verschueren.sofie@telenet.be>
2021-03-18 11:25:09 +0100

    Kan waarde doorsturen, deze code is compleet nu

--- client_01/src/main.cpp
+++ client_01/src/main.cpp
@@ -4,6 +4,7 @@
 #include <RHReliableDatagram.h>
 
 #define MEASUREMENT_PERIOD  5000
+#define SENDING_THRESHOLD   10
 
 #define CLIENT_ADDRESS      2
 #define SERVER_ADDRESS      1
@@ -11,16 +12,16 @@
 #define PIN_SENSOR          10
 #define PIN_3V3_ENABLE      8
 
+//modem
 // Singleton instance of the radio driver
 RH_RF95 driver(6, 2);
-
 // Class to manage message delivery and receipt, using the driver declared above
 RHReliableDatagram manager(driver, CLIENT_ADDRESS);
 
 // flow rate
 volatile uint16_t pulseCount;  
 float calibrationFactor = 98;
-uint8_t totalMilliLitres;
+uint16_t totalMilliLitres;
 unsigned long oldTime=0;
 
 //ISR
@@ -30,10 +31,8 @@ bool prevPulsState = true;
 ISR (PCINT0_vect){ //  pin change interrupt for D8 to D13
   PCIFR  |= bit (digitalPinToPCICRbit(PIN_SENSOR)); // clear any outstanding interrupt
   bool currentState = (bool)digitalRead(PIN_SENSOR);
-  if(prevPulsState){
-    if(!currentState){
+  if(prevPulsState && !currentState ){
       pulseCount++;
-    }
   }
   prevPulsState = currentState;
 }
@@ -78,16 +77,16 @@ void setup()
 
 void loop()
 {
-  if((millis() - oldTime) > MEASUREMENT_PERIOD)    // Only process counters once per minute 
+  if((millis() - oldTime) > MEASUREMENT_PERIOD)     
   {
     oldTime = millis();
     calculatingFlow(); 
   }
 
-  if (totalMilliLitres > 20 ) // send message each 20ml
+  if (totalMilliLitres >= SENDING_THRESHOLD  ) 
   {
-    uint8_t totalMilliLitres_temp= totalMilliLitres; 
-    uint8_t data[] = {totalMilliLitres};
+    uint16_t totalMilliLitres_temp= totalMilliLitres; 
+    uint8_t data[2] = {totalMilliLitres_temp, (totalMilliLitres_temp >> 8)};
     if (sendData(data, sizeof(data)))
     {
     totalMilliLitres -= totalMilliLitres_temp;
@@ -97,7 +96,6 @@ void loop()
 
 bool sendData(uint8_t * data, uint8_t len){
   Serial.println(F("Sending to rf95_server"));
- 
   // Send a message to manager_server
   if (manager.sendtoWait(data, len, SERVER_ADDRESS))
   {
@@ -115,22 +113,17 @@ void calculatingFlow(void)
 {
     //Store counter temporarily
     uint16_t pulseCount_temp = pulseCount;
-
     // Reset the pulse counter so we can start incrementing again 
     pulseCount = 0;
-  
+
     float flowRate =  (float)(pulseCount_temp / calibrationFactor); // calculating per minute
-   
-   
-   // flowMilliLitres = (flowRate / 60) * 1000;   // calculating per seconde 
     float flowMilliLitres = (flowRate *1000  * MEASUREMENT_PERIOD)/ 60000 ;         // calculating per minute
     
     // Add the millilitres passed in this second to the cumulative total
     totalMilliLitres += (uint16_t)round(flowMilliLitres);
-      
     
     // Print the cumulative total of litres flowed since starting
-    Serial.print("Output Liquid Quantity: ");        
+    Serial.print("Liquid Quantity: ");        
     Serial.print(totalMilliLitres);
     Serial.println("mL"); 
 }
